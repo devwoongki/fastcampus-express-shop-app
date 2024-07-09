0fbc7a7e2608ac196da4d63cffa75dab007b90fc
devwoongki <devaglioeolio@gmail.com>
2024-07-09 22:52:01 +0900

    day2

--- src/routers/admin-products.router.js
+++ src/routers/admin-products.router.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const {checkAdmin} = require('../middleware/auth');
 const router = express.Router();
+const Category = require('../models/categories.model');
+router.get('/add-product',checkAdmin, async (req, res, next) => {
+    try{
+        const categories = await Category.find();
+        res.render('admin/add-product',{
+            categories
+        })
+    }catch(err){
+        console.log(err);
+        next(err);
+    }
+})
 
 module.exports = router;
