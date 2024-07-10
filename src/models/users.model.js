4fdebd922f168d2e57a0c4479844354c9b25138e
devwoongki <devaglioeolio@gmail.com>
2024-07-10 23:01:10 +0900

    day3
    product update delete

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -28,33 +28,37 @@ const userSchema = mongoose.Schema({
 })
 
 const saltRounds = 10;
-userSchema.pre('save', function(next){
+userSchema.pre('save', function (next) {
     let user = this;
-    //비밀번호가 변경될때만
-    if(user.isModified('password')){
-        //salt생성
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            if(err) return next(err);
+    // 비밀번호가 변경될 때만
+    if (user.isModified('password')) {
+        // salt를 생성합니다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
 
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err);
-                user.password =  hash;
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
                 next();
             })
         })
-    }else{
+    } else {
         next();
-    }    
+    }
 })
 
-userSchema.methods.comparePassword = function(plainPassword, cb){
-    //bcrypt compare 비교
-    //plain password-> client, this.password > database
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if(err) return cb(err);
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // bcrypt compare 비교 
+    // plain password  => client , this.password => 데이터베이스에 있는 비밀번호
+
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
         cb(null, isMatch);
     })
 }
+
+
 
 const User = mongoose.model('User', userSchema);
 
--- src/server.js
+++ src/server.js
@@ -1,35 +1,44 @@
-const express = require('express');
-const path = require('path');
-const {default : mongoose} = require('mongoose');
-const User = require('./models/users.model');
 const cookieSession = require('cookie-session');
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const passport = require('passport');
+const app = express();
+const path = require('path');
 const flash = require('connect-flash');
 
 const config = require('config');
-const mainRouter = require('./routers/main.router');
-const usersRouter = require('./routers/users.router');
-const productsRouter = require('./routers/products.router');
-const cartRouter = require('./routers/cart.router');
-const adminCategoriesRouter = require('./routers/admin-categories.router');
-const adminProductsRouter = require('./routers/admin-products.router');
-const {checkAdmin} = require('./middleware/auth');
-const methodOverride = require('method-override');
-
 const serverConfig = config.get('server');
-const passport = require('passport');
 
+const mainRouter = require('./routes/main.router');
+const usersRouter = require('./routes/users.router');
+const productsRouter = require('./routes/products.router');
+const cartRouter = require('./routes/cart.router');
+const adminCategoriesRouter = require('./routes/admin-categories.router');
+const adminProductsRouter = require('./routes/admin-products.router');
+const { checkAdmin } = require('./middleware/auth');
+const methodOverride = require('method-override');
+const session = require('express-session');
 const port = serverConfig.port;
+const fileUpload = require('express-fileupload');
 
-require('dotenv').config();
+require('dotenv').config()
 
 
-const app = express();
+app.use(session({
+    secret: 'superSecret',
+    cookie: {
+        httpOnly: true,
+        secure: false
+    },
+    name: 'shop-app-cookie',
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.use(cookieSession({
     name: 'cookie-session-name',
     keys: [process.env.COOKIE_ENCRYPTION_KEY]
-}));
-
+}))
 
 // register regenerate & save after the cookieSession middleware initialization
 app.use(function (request, response, next) {
@@ -48,6 +57,8 @@ app.use(function (request, response, next) {
 
 app.use(flash());
 app.use(methodOverride('_method'));
+
+app.use(fileUpload());
 app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport');
@@ -55,25 +66,24 @@ require('./config/passport');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//view engine config
-
-app.set('views', path.join(__dirname,'views'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('Connected to MongoDB')
-})
-.catch((err) => {
-    console.log(err);
-});
+    .then(() => {
+        console.log('mongodb connected')
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use((req, res, next) => {
+    res.locals.cart = req.session.cart;
     res.locals.error = req.flash('error');
     res.locals.success = req.flash('success');
     res.locals.currentUser = req.user;
@@ -81,18 +91,19 @@ app.use((req, res, next) => {
 })
 
 app.use('/', mainRouter);
-app.use('/auth',usersRouter);
-app.use('/admin/categories',adminCategoriesRouter);
-app.use('/admin/products',adminProductsRouter);
-app.use('/products',productsRouter);
-app.use('/cart',cartRouter);
+app.use('/auth', usersRouter);
+app.use('/products', productsRouter);
+app.use('/cart', cartRouter);
+
+app.use('/admin/products', adminProductsRouter);
+app.use('/admin/categories', adminCategoriesRouter);
 
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.send(err.message || '에러가 났습니다.');
 })
 
-app.listen(port, () => { 
-    console.log(`Server running on port ${port}`);
-});
 
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+})
